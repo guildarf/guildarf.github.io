deb8a38c9a21928efd48ebcf418d3d714c76d2ab
guildarf <guildarfsoftware@gmail.com>
2024-02-07 19:04:22 +0100

    Temporary fix to save not working

--- CookieClicker/IdleTrading.js
+++ CookieClicker/IdleTrading.js
@@ -16,8 +16,8 @@ IdleTrading.launch = function(){
 		for(var iG = 2; iG < Game.ObjectsN; iG++){
 			conf.goods.push({
 				active: true,
-				buyThresh: -1,
-				sellThresh: -1,
+				buyThresh: 1,
+				sellThresh: 150,
 				minPrice: 99999,
 				maxPrice:-99999
 			});
